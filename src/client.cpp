dd60a985437bd3dadc592cfef6eb461b861e2129
Bernardo Hummes <hummescmpa@gmail.com>
2020-09-15 19:15:36 -0300

    gettings chars but messing with line

--- src/client.cpp
+++ src/client.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ncurses.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -17,7 +18,7 @@ typedef int SOCKET;
 
 using namespace std;
 
-void parse_args(int argc, char* argv[], bool *help, bool *verbose, int *port);
+void parse_args(int argc, char* argv[], bool *help, bool *verbose, int *port, char hostname[]);
 void show_help();
 
 int main(int argc, char *argv[])
@@ -26,12 +27,13 @@ int main(int argc, char *argv[])
   bool verbose = true;
   bool help = false;
   int port_no = DEFAULT_PORT;
+  char hostname[256];
 
   // reads arguments
-  parse_args(argc, argv, &help, &verbose, &port_no);
+  parse_args(argc, argv, &help, &verbose, &port_no, hostname);
 
-  if (argc < 1) { // will never go here but keeping to expandability
-    cout << "Too few arguments." << endl;
+  if (argc < 2) { 
+    cout << "Too few arguments, at least specify the hostname." << endl;
     show_help();
     return -1;
   }
@@ -41,59 +43,50 @@ int main(int argc, char *argv[])
   }
  
   if (starting_condition) {
-    cout << "Starting client" << endl;
+    cout << "Starting client for " << hostname << ":" << port_no << endl;
 
-    SOCKET sockfd, clifd;
+    SOCKET sockfd;
     char buffer[256];
     memset(buffer, '\0', sizeof(char)*BUFFER_SIZE);
 
-    struct sockaddr_in serv_addr, cli_addr;
+    struct sockaddr_in serv_addr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);  // start socket with ipv4, tcp, default
-    if (sockfd == -1 && verbose) {
+    if (sockfd == -1) {
       cout << "Error creating socket" << endl;
       return -1;
-    }
-    else if (verbose)
+    } else if (verbose)
       cout << "Success creating socket" << endl;
-
-    const int opt = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); // set option in order not to fail relaunch
 
     // configuring the server address structure
     serv_addr.sin_family      = AF_INET;                         // byte order for ipv4
     serv_addr.sin_port        = htons((unsigned short)port_no);  // port number converted from unsigned short to network byte order
-    serv_addr.sin_addr.s_addr = INADDR_ANY;                      // current hosts ip
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");                      // current hosts ip
 
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1 && verbose) {
-      cout << "Problem binding socket." << endl; // binds the socket to the address and port number
-      return -1;
-    }
-    else if (verbose)
-      cout << "Success binding socket to address " << inet_ntoa(serv_addr.sin_addr) << " at port " <<htons(serv_addr.sin_port) << endl;
-
-    listen(sockfd, QUEUE_SIZE);  // starts queueing at most QUEUE_SIZE connections
-
-    socklen_t socklen = sizeof(cli_addr);
-    clifd = accept(sockfd, (struct sockaddr *)&cli_addr, &socklen);  // takes address of first connection in the queue
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+      cout << "Error connecting to host" << endl;
+    else if(verbose)
+      cout << "received connection from " << inet_ntoa(serv_addr.sin_addr) << " at port " << ntohs(serv_addr.sin_port) << endl;
     
-    if (verbose)
-      cout << "received connection from " << inet_ntoa(cli_addr.sin_addr) << " at port " << ntohs(cli_addr.sin_port) << endl;
-    
-    send(clifd, buffer, sizeof(buffer), 0); // sends welcome message to new connection
+    char letra[1];
+    system("stty raw");
+    do {
+      letra[0] = getchar();
+      send(sockfd, letra, sizeof(letra), 0); // sends welcome message to new connection
+      printf("%c", letra[0]);
+    } while (letra[0] != ' ');
+    system("stty cooked");
+
+    send(sockfd, buffer, sizeof(buffer), 0); // sends welcome message to new connection
 
     memset(buffer, '\0', sizeof(char)*BUFFER_SIZE);
-    if (recv(clifd, buffer, sizeof(buffer), 0) == -1) // receives input from connection
+    if (recv(sockfd, buffer, sizeof(buffer), 0) == -1) // receives input from connection
       cout << "Error reading message from socket" << endl;
-    else
+    else if (verbose)
       cout << "Received message: " << buffer << endl;
 
-    send(clifd, buffer, sizeof(buffer), 0); // sends welcome message to new connection
+    send(sockfd, buffer, sizeof(buffer), 0); // sends welcome message to new connection
 
-
-    
-    shutdown(clifd, SHUT_RDWR);
-    close(clifd);
     shutdown(sockfd, SHUT_RDWR);
     close(sockfd);
   }
@@ -101,7 +94,7 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-void parse_args(int argc, char* argv[], bool *help, bool *verbose, int *port) {
+void parse_args(int argc, char* argv[], bool *help, bool *verbose, int *port, char hostname[]) {
   int i = 0;
   while(i < argc){
     if(!strcmp(argv[i], "-v") || !strcmp(argv[i], "--verbose")) {
@@ -109,6 +102,12 @@ void parse_args(int argc, char* argv[], bool *help, bool *verbose, int *port) {
     }
     if(!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
       *help = true;
+    }
+    else if(!strcmp(argv[i], "-n") || !strcmp(argv[i], "--hostname")){
+      i++;
+      strcat(argv[i],"\0");
+      if(i < argc)
+        strcpy(hostname, argv[i]);
     }
     else if(!strcmp(argv[i], "-p") || !strcmp(argv[i], "--port")){
       i++;
@@ -123,5 +122,6 @@ void show_help() {
   cout << "Command line options:" << endl << endl;
   cout << "( -h  | --help )              : Displays help message" << endl;
   cout << "( -v  | --verbose )           : Displays more processing information" << endl;
-  cout << "( -p  | --port) <N>           : Port to which bind the socket" << endl;
+  cout << "( -n  | --hostname) <STRING>  : Hostname of the server to connect" << endl;
+  cout << "( -p  | --port) <N>           : Port to which connect in the informed hostname" << endl;
 }
